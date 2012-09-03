6cabf88bbb407e9f0a290e1fdec4a886d2959ac5
Andrae Muys <andrae.muys@gmail.com>
2012-09-03 16:38:26 +1000

    Added broken tests; will need to get these working and then I will have an operational base project.

---
+++ src/test/java/net/qldarch/test/web/service/EmbeddedTestServer.java
@@ -0,0 +1,29 @@
+package net.qldarch.test.web.service;
+
+import org.junit.rules.ExternalResource;
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.webapp.WebAppContext;
+
+public class EmbeddedTestServer extends ExternalResource {
+
+    Server server;
+    int port = 8080;
+
+    @Override
+    protected void before() throws Throwable {
+        server = new Server(port);
+        server.addHandler(new WebAppContext("src/main/webapp", "/"));
+        server.start();
+    }
+
+    @Override
+    protected void after() {
+        try {
+            server.stop();
+        } catch (Throwable t) {}
+    }
+
+    public String uri() {
+        return "http://localhost:" + port;
+    }
+}
