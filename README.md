f2b5744a5b7f79d34fbad931e2bdadc396ebc009
Andrae Muys <andrae.muys@gmail.com>
2012-09-03 13:58:13 +1000

    Initial commit; working HelloWorld example with build environment and deploy/undeploy targets

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a sample project as an example of an ant-driven build using a maven standard directory layout.
---
+++ src/main/java/net/qldarch/web/service/HelloWorldResource.java
@@ -0,0 +1,15 @@
+package net.qldarch.web.service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/hello")
+public class HelloWorldResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello World!";
+    }
+
+}
