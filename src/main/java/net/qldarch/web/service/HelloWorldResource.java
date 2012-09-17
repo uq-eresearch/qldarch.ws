1bf260edb11d72f713ce116a67282f251596bce1
Andrae Muys <andrae.muys@gmail.com>
2012-09-17 16:25:05 +1000

    Keeping things uptodate.

--- src/main/java/net/qldarch/web/service/HelloWorldResource.java
+++ src/main/java/net/qldarch/web/service/HelloWorldResource.java
@@ -4,12 +4,20 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Path("/hello")
 public class HelloWorldResource {
+    public static Logger logger = LoggerFactory.getLogger(HelloWorldResource.class);
+    
+    public static String message = "Hello World!";
+
     @GET
     @Produces("text/plain")
     public String hello() {
-        return "Hello World!";
+        logger.warn("Returning {} from GET", message);
+        return message;
     }
 
 }
