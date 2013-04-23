964e70326a874a62c9bc463615555444fd38d257
Andrae Muys <andrae.muys@gmail.com>
2013-04-23 10:43:35 +1000

    Cleaned up imports and unused members of InterviewSummaryResource

--- src/main/java/net/qldarch/web/service/InterviewSummaryResource.java
+++ src/main/java/net/qldarch/web/service/InterviewSummaryResource.java
@@ -1,28 +1,15 @@
 package net.qldarch.web.service;
 
-import org.openrdf.model.Literal;
-import org.openrdf.model.Value;
-import org.openrdf.query.*;
-import org.openrdf.repository.Repository;
-import org.openrdf.repository.RepositoryConnection;
-import org.openrdf.repository.RepositoryException;
-import org.openrdf.repository.http.HTTPRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.util.HashMap;
-import java.util.Map;
 
 @Path("/interviewSummary")
 public class InterviewSummaryResource {
     public static Logger logger = LoggerFactory.getLogger(InterviewSummaryResource.class);
-    public static final String XSD_BOOLEAN = "http://www.w3.org/2001/XMLSchema#boolean";
-
 
     @GET
     @Produces("application/json")
