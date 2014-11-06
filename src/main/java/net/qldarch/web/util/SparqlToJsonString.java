8aa30d6dc77595d1b65f3ffc21b07fc0afa62724
Andre Gebers <a.gebers@uq.edu.au>
2014-11-07 09:10:45 +1000

    remove tabs from string. fixes #9

--- src/main/java/net/qldarch/web/util/SparqlToJsonString.java
+++ src/main/java/net/qldarch/web/util/SparqlToJsonString.java
@@ -169,6 +169,6 @@ public class SparqlToJsonString {
 
     private String cleanString(String in) {
         return in.replaceAll("\n", "").replaceAll("\r", "").replaceAll("\\\\", "\\\\\\\\")
-        		.replaceAll("\"", "\\\\\"").trim(); 
+            .replaceAll("\"", "\\\\\"").replaceAll("\t", "").trim();
     }
 }
